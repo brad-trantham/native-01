e95ec7c5e7b41c4cea3e371dfccebadaa5f8633d
Brad Trantham <btrantham@swri.org>
2020-09-11 15:05:11 -0500

    Commit after lecture 245

--- components/MapPreview.js
+++ components/MapPreview.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Image, View, StyleSheet } from 'react-native'
+import { TouchableOpacity, Image, View, StyleSheet } from 'react-native'
 
 import ENV from '../env'
 
@@ -9,9 +9,9 @@ const MapPreview = props => {
         imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${props.location.lat},${props.location.lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:A%7C${props.location.lat},${props.location.lng}&key=${ENV.googleApiKey}`
     }
 
-    return <View style={{...styles.mapPreview, ...props.style}}>
+    return <TouchableOpacity onPress={props.onPress} style={{...styles.mapPreview, ...props.style}}>
         {props.location ? <Image style={styles.mapImage} source={{uri: imagePreviewUrl}}/>: props.children}
-    </View>
+    </TouchableOpacity>
 }
 
 const styles = StyleSheet.create({
